3929028868c6b5b7b4586095e3b134c121523a8d
noahwekesa <noahwekesa0@gmail.com>
2023-12-27 18:59:25 +0300

    executable

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+script that starts a flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    """
+Return Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+Return HBNB on /hbnb route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_cool(text):
+    """
+Return C followed by the value the text variable
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>')
+def python_cool(text):
+    """
+    Return the default value of text is "is cool"
+    """
+    return "python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<n>')
+def diplay_number(n):
+    """
+return n if only n is an integer
+    """
+    return "{} is a number".format(int(n))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
--- web_flask/exec.sh
+++ web_flask/exec.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
-chmod +x *.py
+for file in *.py; do
+	if [ -f "$file" ]; then
+		if [ ! -x "$file" ]; then
+			chmod +x "$file"
+			echo "Made $file executable."
+		else
+			echo "$file is already executable."
+		fi
+	fi
+done
